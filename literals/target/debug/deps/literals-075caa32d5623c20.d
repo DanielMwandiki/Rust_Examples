67f9dc99385f98d8b4c4d1915bb18b8fb8af4861
Daniel MWandiki <danmasketcha@gmail.com>
2024-07-24 16:34:58 +0300

    tuples

---
+++ literals/target/debug/deps/literals-075caa32d5623c20.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/danma/Desktop/Rust_Examples/literals/target/debug/deps/literals-075caa32d5623c20: src/main.rs
+
+/mnt/c/Users/danma/Desktop/Rust_Examples/literals/target/debug/deps/literals-075caa32d5623c20.d: src/main.rs
+
+src/main.rs:
---
+++ literals/target/debug/literals.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/danma/Desktop/Rust_Examples/literals/target/debug/literals: /mnt/c/Users/danma/Desktop/Rust_Examples/literals/src/main.rs
