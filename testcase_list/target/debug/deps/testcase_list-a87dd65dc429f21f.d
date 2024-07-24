f6eb043463eca073e2dc473140265dbdb4c2e1f6
Daniel MWandiki <danmasketcha@gmail.com>
2024-07-24 06:37:58 +0300

    testcase

---
+++ testcase_list/target/debug/deps/testcase_list-a87dd65dc429f21f.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/danma/Desktop/Rust_Examples/testcase_list/target/debug/deps/testcase_list-a87dd65dc429f21f: src/main.rs
+
+/mnt/c/Users/danma/Desktop/Rust_Examples/testcase_list/target/debug/deps/testcase_list-a87dd65dc429f21f.d: src/main.rs
+
+src/main.rs:
---
+++ testcase_list/target/debug/testcase_list.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/danma/Desktop/Rust_Examples/testcase_list/target/debug/testcase_list: /mnt/c/Users/danma/Desktop/Rust_Examples/testcase_list/src/main.rs
