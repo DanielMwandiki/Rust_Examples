55f91b2323ee36149c8ba452a27fd2d9acdead33
Daniel MWandiki <danmasketcha@gmail.com>
2024-07-24 06:43:46 +0300

    formatting

---
+++ formatting/target/debug/deps/formatting-9b15fa861dfe2be1.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/danma/Desktop/Rust_Examples/formatting/target/debug/deps/formatting-9b15fa861dfe2be1: src/main.rs
+
+/mnt/c/Users/danma/Desktop/Rust_Examples/formatting/target/debug/deps/formatting-9b15fa861dfe2be1.d: src/main.rs
+
+src/main.rs:
---
+++ formatting/target/debug/formatting.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/danma/Desktop/Rust_Examples/formatting/target/debug/formatting: /mnt/c/Users/danma/Desktop/Rust_Examples/formatting/src/main.rs
